31b972cc1e9387a4a6d019b6702ca016b0c48d51
Paul Hendrick <paul@errorstudio.co.uk>
2016-07-13 11:50:52 +0100

    return empty arrays in decorator methods

--- lib/rooftop/rails/events/decorators/cast.rb
+++ lib/rooftop/rails/events/decorators/cast.rb
@@ -6,12 +6,16 @@ module Rooftop
         def cast_members
           if object.has_field?(:cast, Array)
             object.fields.cast.collect{|cm| PersonDecorator.new(cm)}
+          else
+            []
           end
         end
 
         def creatives
           if object.has_field?(:creatives, Array)
             object.fields.creatives.collect{|cm| PersonDecorator.new(cm)}
+          else
+            []
           end
         end
 
--- lib/rooftop/rails/events/version.rb
+++ lib/rooftop/rails/events/version.rb
@@ -1,7 +1,7 @@
 module Rooftop
   module Rails
     module Events
-      VERSION = "0.1.3"
+      VERSION = "0.1.4"
     end
   end
 end
